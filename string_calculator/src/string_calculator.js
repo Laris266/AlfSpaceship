5fedf4426e8450c506009440f21bede3e916625e
Laris266 <if25b266@technikum-wien.at>
2026-06-20 14:27:02 +0200

    4. test green for [2,1]

--- string_calculator/src/string_calculator.js
+++ string_calculator/src/string_calculator.js
@@ -1,11 +1,14 @@
 
 function SpaceshipFinder() {}
-SpaceshipFinder.prototype.findSpaceship = function(string) {
-	if(string === 'X') {
-		return [0, 0];
-	}
-	if(string === '.X') {
-		return [1, 0];
+SpaceshipFinder.prototype.findSpaceship = function(map) {
+	const rows = map.split('\n');
+
+	for (let y = 0; y < rows.length; y++) {
+		const row = rows[y];
+		const x = row.indexOf('X');
+		if (x !== -1) {
+			return [x, y];
+		}
 	}
 	return "Spaceship lost forever.";
 };
