1954c6ab9d464f882b060d0afef482c781db1ac1
Laris266 <if25b266@technikum-wien.at>
2026-06-20 13:32:55 +0200

    2. failing test for 'X'

--- string_calculator/spec/string_calculator_spec.js
+++ string_calculator/spec/string_calculator_spec.js
@@ -1,11 +1,14 @@
 describe('SpaceshipFinder', function() {
 	let finder;
-	
+
 	beforeEach(function() {
 		finder = new SpaceshipFinder();
 	});
 
 	it('should return "Spaceship lost forever." for an empty string', function() {
 		expect(finder.findSpaceship('')).toBe("Spaceship lost forever.");
+	});
+	it('should return [0,0] for "X"', function() {
+		expect(finder.findSpaceship('X')).toEqual([0,0]);
 	});
 });
