3d7e6fe850b4daddc92b560339ff767429ea34fa
Laris266 <if25b266@technikum-wien.at>
2026-06-20 14:01:59 +0200

    3. test green for '.X'

--- string_calculator/src/string_calculator.js
+++ string_calculator/src/string_calculator.js
@@ -4,5 +4,8 @@ SpaceshipFinder.prototype.findSpaceship = function(string) {
 	if(string === 'X') {
 		return [0, 0];
 	}
+	if(string === '.X') {
+		return [1, 0];
+	}
 	return "Spaceship lost forever.";
 };
